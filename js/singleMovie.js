81ee7baadc38a28cd4bda707ff8c5a4be453f051
Nick <nick.belenchuk@gmail.com>
2023-06-21 19:43:33 +0300

    fixed singleMovie

--- js/singleMovie.js
+++ js/singleMovie.js
@@ -1,42 +1,68 @@
 import { data } from "./data.js";
+import { burger } from "./burger.js";
 
-export const createMovieContainer = (data) => {
+export const createMovieContainer = (singleCardData) => {
   const container = document.createElement("div");
   container.classList.add("container", "movie__container");
 
+  const similarMovies = data.filter((movie) =>
+    movie.genre.some((genre) => singleCardData.genre.includes(genre))
+  );
+
+  const similarMoviesHTML = similarMovies
+    .map(
+      (movie) =>
+        `<a href="./singleMovie.html?id=${movie.id}"><img class="similar__movies-img" src="${movie.image}" alt="img" /></a>`
+    )
+    .join("");
+
   const movieHTML = `
-    <div class="movie__block-header">
-      <h1 class="movie__header">${data.title}</h1>
+        <div class="movie__block-header">
+      <h1 class="movie__header">${singleCardData.title}</h1>
       <div class="movie__rating">
         <p class="rating">
-          Rating: <span class="rating__current">${data.rating}</span>
+        <span class="year"> ${singleCardData.year}</span>
+          Rating: <span class="rating__current">${singleCardData.rating}</span>
           <span class="rating__max"> / 10</span>
         </p>
       </div>
     </div>
     <div class="movie__block-content">
-      <img class="movie__block-img" src="${data.image}" alt="movie" />
+      <img class="movie__block-img" src="${singleCardData.image}" alt="movie" />
+      
       <div class="about__movie">
-        <p class="about__movie-text">${data.description}</p>
+        <p class="about__movie-text">${singleCardData.description}</p>
         <div class="about__movie-genre">
-          <span class="genre">${data.genre}</span>
+          ${singleCardData.genre
+            .map((genre) => `<span class="genre">${genre}</span>`)
+            .join("")}
         </div>
         <div class="about__movie-cast">
-          <h4 class="cast__header">Cast:</h4>
-          <div class="cast__block"></div>
+          <h4 class="cast__header">Director:</h4>
+          <div class="cast__block"><div class="cast__block-hero">
+          <span class="cast__hero-name">${
+            singleCardData.director
+          }</span></div></div>
+        </div>
+        <div class="about__movie-cast">
+          <h4 class="cast__header">Writers:</h4>
+          <div class="cast__block"><div class="cast__block-hero">
+          ${singleCardData.writers
+            .map((writer) => `<span class="cast__hero-name">${writer}</span>`)
+            .join("")}</div></div>
+            <a class="movie__block-watch" target="_blank" href="${
+              singleCardData.trailer
+            }">whatch trailer</a>
         </div>
       </div>
-      <form class="popup" action="">
-        <h5>Rate this</h5>
-        <h4>Sharper</h4>
-        <input class="popup__input" type="number" max="10" min="0" />
-        <button class="popup__btn" type="submit">rate</button>
-      </form>
     </div>
+    
     <div class="movie__block-similar">
       <h4 class="similar__header">Similar movies</h4>
       <div class="similar__movies">
-        <div class="similar__movies-block"></div>
+        <div class="similar__movies-block">
+          ${similarMoviesHTML}
+        </div>
       </div>
     </div>
   `;
@@ -45,6 +71,7 @@ export const createMovieContainer = (data) => {
 
   return container;
 };
+
 const params = new URLSearchParams(window.location.search);
 const movieIndex = params.get("id");
 const movieData = data.find((movie) => movie.id === movieIndex);
@@ -55,3 +82,5 @@ if (movieData) {
 } else {
   console.log("Invalid movie id");
 }
+
+burger();
